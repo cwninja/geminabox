7d435e36f50cf1ee8c55571a4ac0e72a3e0db3db
Tom Lea <commit@tomlea.co.uk>
2015-11-26 22:19:54 +0000

    Extract base server from app code.

--- spec/support/test_server.rb
+++ spec/support/test_server.rb
@@ -1,80 +1,97 @@
 require 'net/http'
 require 'webrick'
 
-class TestServer
-  attr_reader :gem_store
-
-  # Create a newGemStore server
+module TestServer
+  # Create a new GemStoreServer
   def self.gem_store(&block)
-    new(&block)
+    GemStoreServer.new(&block)
   end
 
-  def initialize(&block)
-    @fixture_setup = block
-  end
+  class Generic
+    def run
+      start_server!
+      wait_until_booted!
+      yield
+      cleanup!
+    end
 
-  def run
-    create_gemstore!
-    start_server!
-    setup_gems!
-    wait_until_booted!
-    yield
-    cleanup!
-  end
+    def cleanup!
+      Thread.kill(@thread)
+    end
 
-  def url
-    "http://127.0.0.1:#{@port}"
-  end
+    def url
+      "http://127.0.0.1:#{@port}"
+    end
 
-  def http_client(&block)
-    uri = URI(url)
-    Net::HTTP.start(uri.host, uri.port, &block)
-  end
+    def http_client(&block)
+      uri = URI(url)
+      Net::HTTP.start(uri.host, uri.port, &block)
+    end
 
-protected
-  def create_gemstore!
-    @dir = Pathname.new(Dir.mktmpdir)
-    @gem_store = Geminabox::GemStore(@dir)
-  end
+  protected
+    def start_server!
+      @port = find_available_port
+      @thread = Thread.start do
+        Rack::Handler::WEBrick.run app, Port: @port, Host: '127.0.0.1'
+      end
+    end
 
-  def setup_gems!
-    gemset_factory = GemsetFactory.new(gem_store)
-    @fixture_setup.call(gemset_factory)
-  end
+    def find_available_port
+      server = TCPServer.new('127.0.0.1', 0)
+      server.addr[1]
+    ensure
+      server.close if server
+    end
 
-  def start_server!
-    @port = find_available_port
-    @thread = Thread.start do
-      app = Geminabox::app(@dir)
-      Rack::Handler::WEBrick.run app, Port: @port, Host: '127.0.0.1'
+    def booted?
+      ::Net::HTTP.get_response("127.0.0.1", '/', @port)
+      return true
+    rescue Errno::ECONNREFUSED, Errno::EBADF
+      return false
+    end
+
+    def wait_until_booted!
+      start_time = Time.now
+      loop do
+        return if booted?
+        raise TimeoutError.new if (Time.now - start_time) > 2
+        sleep(0.05)
+      end
     end
   end
 
-  def find_available_port
-    server = TCPServer.new('127.0.0.1', 0)
-    server.addr[1]
-  ensure
-    server.close if server
-  end
+  class GemStoreServer < Generic
+    attr_reader :gem_store
 
-  def booted?
-    ::Net::HTTP.get_response("127.0.0.1", '/', @port)
-    return true
-  rescue Errno::ECONNREFUSED, Errno::EBADF
-    return false
-  end
+    def initialize(&block)
+      @fixture_setup = block
+    end
 
-  def wait_until_booted!
-    start_time = Time.now
-    loop do
-      return if booted?
-      raise TimeoutError.new if (Time.now - start_time) > 2
-      sleep(0.05)
+    def run(&block)
+      create_gemstore!
+      setup_gems!
+      super(&block)
+    end
+
+  protected
+    def app
+      Geminabox::app(@dir)
+    end
+
+    def create_gemstore!
+      @dir = Pathname.new(Dir.mktmpdir)
+      @gem_store = Geminabox::GemStore(@dir)
+    end
+
+    def setup_gems!
+      gemset_factory = GemsetFactory.new(gem_store)
+      @fixture_setup.call(gemset_factory)
+    end
+
+    def cleanup!
+      super
+      FileUtils.remove_entry @dir
     end
   end
 
-  def cleanup!
-    Thread.kill(@thread)
-    FileUtils.remove_entry @dir
-  end
 end

095045d12788d12e6ae5bc6fa66c3ae17f3ee7f2
Tom Lea <commit@tomlea.co.uk>
2015-11-26 13:58:20 +0000

    Work with RBX?

--- lib/geminabox/spec_extractor.rb
+++ lib/geminabox/spec_extractor.rb
@@ -3,12 +3,14 @@ require "yaml"
 
 Geminabox::SpecExtractor = ->(io){
   raise ArgumentError, "Bad io object" unless io.respond_to? :read
+  spec = nil
   Archive::Tar::Minitar::Reader.open(io) do |tar|
     tar.each do |entry|
       if entry.full_name == "metadata.gz"
-        return YAML.load(Zlib::GzipReader.new(entry).read)
+        spec = YAML.load(Zlib::GzipReader.new(entry).read)
+        break
       end
     end
   end
-  raise Geminabox::BadGemfile.new("Could not find a metadata.rz file")
+  spec or raise Geminabox::BadGemfile.new("Could not find a metadata.rz file")
 }
